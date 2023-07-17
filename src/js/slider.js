dc579388262b6864be88fedfdd6b9a5aa8937a0c
SoT03 <bogdan.zygadlo123@gmail.com>
2023-07-17 21:25:54 +0200

    created slider

---
+++ src/js/slider.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector('.slider');
+const sliderImges = document.querySelectorAll('.slider__box');
+const leftBtn = document.querySelector('.slider__btn--left');
+const rightBtn = document.querySelector('.slider__btn--right');
+const sliderSpeed = 3000;
+
+let prev;
+let index = 0;
+
+const handleCarousel = () => {
+	index++;
+	changeImg();
+};
+
+let startCarousel = setInterval(handleCarousel, sliderSpeed);
+
+const changeImg = () => {
+	if (index > sliderImges.length - 1) {
+		index = 0;
+	} else if (index < 0) {
+		index = sliderImges.length - 1;
+	}
+
+	sliderImges.forEach((img) => {
+		console.log(img.getAttribute('data-number'));
+		if (img.getAttribute('data-number') != index) {
+			img.classList.remove('show');
+		} else {
+			img.classList.add('show');
+		}
+	});
+};
+
+const handleRightArrow = () => {
+	index++;
+	resetInterval();
+};
+
+const handleLeftArrow = () => {
+	index--;
+	resetInterval();
+};
+
+const resetInterval = () => {
+	changeImg();
+	clearInterval(startCarousel);
+	startCarousel = setInterval(handleCarousel, sliderSpeed);
+};
+
+rightBtn.addEventListener('click', handleRightArrow);
+leftBtn.addEventListener('click', handleLeftArrow);
